a89cfa79f9a96a8e88b1bc5b2548fe8cce308d48
John Leszczynski <jrleszczynski@gmail.com>
2015-08-29 16:36:06 -0400

    modify tests to use new addVertex methods

--- test/decision-graph.test.js
+++ test/decision-graph.test.js
@@ -7,28 +7,28 @@ test('new DecisionGraph methods', function (t) {
   t.true(dg instanceof DecisionGraph)
   t.equal(dg.V(), 0)
 
-  dg.addVertex('NounPhrase', false)
+  dg.addVertexOR('NounPhrase')
   t.equal(dg.V(), 1)
   t.deepEqual(dg.adj('NounPhrase'), [])
   t.true(dg.isTerminal('NounPhrase'))
   t.false(dg.isTypeAND('NounPhrase'))
 
-  dg.addVertex('NP1', true)
+  dg.addVertexAND('NP1')
   t.equal(dg.V(), 2)
   t.deepEqual(dg.adj('NP1'), [])
   t.true(dg.isTypeAND('NP1'))
 
-  dg.addVertex('NP2')
+  dg.addVertexAND('NP2')
   t.equal(dg.V(), 3)
   t.deepEqual(dg.adj('NP2'), [])
   t.true(dg.isTypeAND('NP2'))
 
-  dg.addVertex('Noun', false)
-  dg.addVertex('RelativeClause')
+  dg.addVertexOR('Noun')
+  dg.addVertexAND('RelativeClause')
 
   var terminals = ['the', 'dog', 'cat', 'bird', 'squirrel']
   terminals.forEach(function (terminal) {
-    dg.addVertex(terminal)
+    dg.addVertexAND(terminal)
   })
   t.equal(dg.V(), 10)
 
