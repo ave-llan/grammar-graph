453e2f2d4b9e2fc1955573172347eb10c0ff9e5c
John Leszczynski <jrleszczynski@gmail.com>
2015-09-01 17:58:15 -0400

    create tests for reduce-grammar

---
+++ test/reduce-grammar.test.js
@@ -0,0 +1,55 @@
+var test = require('tape')
+var reduceGrammar = require('../lib/reduce-grammar.js')
+
+test('reduceGrammar', function (t) {
+  var g = {
+    NounPhrase: ['the Noun', 'the Noun RelativeClause'],
+    RelativeClause: ['that VerbPhrase'],
+    Noun: ['dog', 'cat', 'bird']
+  }
+
+  t.deepEqual(reduceGrammar(g), {
+    NounPhrase: ['_NounPhrase_1', '_NounPhrase_2'],
+    _NounPhrase_1: ['the Noun'],
+    _NounPhrase_2: ['the Noun RelativeClause'],
+    RelativeClause: ['that VerbPhrase'],
+    Noun: ['dog', 'cat', 'bird']
+  })
+
+  g = {
+    Sentence: ['NounPhrase VerbPhrase'],
+    NounPhrase: ['the Noun', 'the Noun RelativeClause'],
+    VerbPhrase: ['Verb', 'Verb NounPhrase'],
+    RelativeClause: ['that VerbPhrase'],
+    Noun: ['dog', 'cat', 'bird', 'squirrel'],
+    Verb: ['befriended', 'loved', 'ate', 'attacked']
+  }
+
+  t.deepEqual(reduceGrammar(g), {
+    Sentence: [ 'NounPhrase VerbPhrase' ],
+    NounPhrase: [ '_NounPhrase_1', '_NounPhrase_2' ],
+    VerbPhrase: [ 'Verb', '_VerbPhrase_1' ],
+    RelativeClause: [ 'that VerbPhrase' ],
+    Noun: [ 'dog', 'cat', 'bird', 'squirrel' ],
+    Verb: [ 'befriended', 'loved', 'ate', 'attacked' ],
+    _NounPhrase_1: [ 'the Noun' ],
+    _NounPhrase_2: [ 'the Noun RelativeClause' ],
+    _VerbPhrase_1: [ 'Verb NounPhrase' ]
+  })
+
+  g = {
+    NounPhrase: ['the+Noun', 'the+Noun+RelativeClause'],
+    RelativeClause: ['that+VerbPhrase'],
+    Noun: ['dog', 'cat', 'bird']
+  }
+
+  t.deepEqual(reduceGrammar(g, '+'), {
+    NounPhrase: ['_NounPhrase_1', '_NounPhrase_2'],
+    _NounPhrase_1: ['the+Noun'],
+    _NounPhrase_2: ['the+Noun+RelativeClause'],
+    RelativeClause: ['that+VerbPhrase'],
+    Noun: ['dog', 'cat', 'bird']
+  })
+
+  t.end()
+})
